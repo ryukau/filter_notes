8374f2ad5c189460b2cea90b04697832b5274b36
cu <cu@localhost.localdomain>
2019-06-24 09:27:39 +0900

    Add header to show last modified time

--- build.py
+++ build.py
@@ -8,6 +8,7 @@ import json
 import os
 import re
 import subprocess
+import time
 import yaml
 
 from bs4 import BeautifulSoup
@@ -28,10 +29,23 @@ def read_build_info(rebuild=False):
 def gather_markdown():
     mds = []
     for path in Path(".").glob("*"):
-        if path.is_dir() and not re.match("^[\._]", str(path)[0]):
+        if path.is_dir() and not re.match(r"^[\._]", str(path)[0]):
             for md in path.glob("*.md"):
                 mds.append(md)
     return mds
+
+def get_last_modified(md):
+    result = subprocess.run(
+        ["git", "log", "-1", '--format="%ai"', "--", md],
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if len(result.stdout) > 0:
+        return result.stdout[1:-1]  # remove double quotation (").
+
+    # if there are no commits yet, get it from system.
+    epoch = Path(md).stat().st_mtime
+    return time.strftime("%Y-%m-%d %H:%M:%S %z", time.localtime(epoch))
 
 def pandoc_md_to_html5(md, md_info, template_html):
     if md.suffix != ".md":
@@ -55,9 +69,11 @@ def pandoc_md_to_html5(md, md_info, template_html):
     result = subprocess.run(
         ["pandoc", "-f", "markdown", "-t", "html5", "--katex", md],
         stdout=subprocess.PIPE,
-        encoding="utf-8")
+        encoding="utf-8",
+    )
 
     html = template_html.replace("<!-- insert title here -->", md.stem)
+    html = html.replace("<!-- last modified -->", get_last_modified(md).split(" ")[0])
     html = html.replace("<!-- replace me -->", result.stdout)
 
     with open(md.with_suffix(".html"), "w") as html_file:
@@ -71,8 +87,7 @@ def dump_config_yml():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-r", "--rebuild", action='store_true', help="rebuild add file")
+    parser.add_argument("-r", "--rebuild", action='store_true', help="rebuild all file")
     args = parser.parse_args()
 
     dump_config_yml()
